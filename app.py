5900c969f18ad3204af1f758d92c355c45368239
Adam Jones <admajonse@gmail.com>
2022-04-13 21:33:54 -0400

    add protocol version to webapp

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from multiprocessing import Process
 from src import bot
 from src import logger
 from time import sleep
+from werkzeug.serving import WSGIRequestHandler
 
 
 app = Flask(__name__, static_folder="app/static/", template_folder="app/static/")
@@ -36,4 +37,5 @@ if __name__ == "__main__":
     Process(target=bot.check_for_updates).start()    
     port = int(os.environ.get('PORT', 5000))
     #serve(app, host="0.0.0.0", port=port, threads=12)
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(host="0.0.0.0", port=port)
